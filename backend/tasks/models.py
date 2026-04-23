82ca5cb06ae0cb2459492f19cb9930c83df5bbd2
Jessee Gatimu <gakuojessee@gmail.com>
2026-04-23 11:22:45 +0300

    Added tasks model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES=(
+        ('completed','Completed'),
+        ('in_progress','In_progress'),
+        ('pending','Pending')
+    )
+    title=models.CharField(max_length=50)
+    description=models.TextField()
+    completion_date=models.DateTimeField()
+    status=models.CharField(choices=STATUS_CHOICES,max_length=20,default='Pending')
+    
+    def __str__(self):
+        return f"{self.title} - {self.status}"
